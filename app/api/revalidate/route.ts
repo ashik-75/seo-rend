c434668f3af6627234295289aa2376252527b2bb
Ashik <ashik@pop-os.localdomain>
2023-06-08 23:47:37 +0600

    update webhooks

--- app/api/revalidate/route.ts
+++ app/api/revalidate/route.ts
@@ -1,3 +1,4 @@
+import swell from "@/lib/swell/client";
 import { revalidatePath } from "next/cache";
 import { NextRequest, NextResponse } from "next/server";
 
@@ -7,36 +8,14 @@ export async function POST(req: NextRequest, res: NextResponse) {
   try {
     const { searchParams } = new URL(req.url);
     const payload = await req.json();
-    console.log({ method: req.method, payload });
+    const product = await swell.products.get(payload?.data?.id);
+    console.log({ method: req.method, payload, product });
 
     const secret = searchParams.get("secret");
 
     if (secret === process.env.REVALIDATE_SECRET) {
       revalidatePath("/");
-      return NextResponse.json({ revalidate: true });
-    } else {
-      return new NextResponse(null, {
-        status: 401,
-        statusText: "Restricted path",
-      });
-    }
-  } catch (error) {
-    return new NextResponse(null, {
-      status: 400,
-      statusText: "something went wrong!",
-    });
-  }
-}
-
-export async function GET(req: NextRequest, res: NextResponse) {
-  try {
-    const { searchParams } = new URL(req.url);
-
-    console.log({ method: req.method });
-    const secret = searchParams.get("secret");
-
-    if (secret === process.env.REVALIDATE_SECRET) {
-      revalidatePath("/");
+      revalidatePath(`/${product?.slug}`);
       return NextResponse.json({ revalidate: true });
     } else {
       return new NextResponse(null, {
