f44d507b66b796ac89adb7d3c826a78a625b455c
Ashik <ashik@pop-os.localdomain>
2023-06-08 23:24:20 +0600

    revalidate path

--- app/api/revalidate/route.ts
+++ app/api/revalidate/route.ts
@@ -6,13 +6,38 @@ export const runtime = "edge";
 export async function POST(req: NextRequest, res: NextResponse) {
   try {
     const { searchParams } = new URL(req.url);
-    const tt = await req.json();
-    console.log(req.method);
+    const payload = await req.json();
+    console.log({ method: req.method, payload });
+
     const secret = searchParams.get("secret");
 
     if (secret === process.env.REVALIDATE_SECRET) {
       revalidatePath("/");
-      return NextResponse.json({ revalidate: true, tt });
+      return NextResponse.json({ revalidate: true });
+    } else {
+      return new NextResponse(null, {
+        status: 401,
+        statusText: "Restricted path",
+      });
+    }
+  } catch (error) {
+    return new NextResponse(null, {
+      status: 400,
+      statusText: "something went wrong!",
+    });
+  }
+}
+
+export async function GET(req: NextRequest, res: NextResponse) {
+  try {
+    const { searchParams } = new URL(req.url);
+    const payload = await req.json();
+    console.log({ method: req.method, payload });
+    const secret = searchParams.get("secret");
+
+    if (secret === process.env.REVALIDATE_SECRET) {
+      revalidatePath("/");
+      return NextResponse.json({ revalidate: true });
     } else {
       return new NextResponse(null, {
         status: 401,
