3da0f2dd5c8f9d1d71e906b50e3c0f56b9122ab0
Ashik <ashik@pop-os.localdomain>
2023-06-08 23:34:34 +0600

    webhook cjhanges

--- app/api/revalidate/route.ts
+++ app/api/revalidate/route.ts
@@ -1,7 +1,7 @@
 import { revalidatePath } from "next/cache";
 import { NextRequest, NextResponse } from "next/server";
 
-export const runtime = "edge";
+// export const runtime = "edge";
 
 export async function POST(req: NextRequest, res: NextResponse) {
   try {
@@ -31,8 +31,8 @@ export async function POST(req: NextRequest, res: NextResponse) {
 export async function GET(req: NextRequest, res: NextResponse) {
   try {
     const { searchParams } = new URL(req.url);
-    const payload = await req.json();
-    console.log({ method: req.method, payload });
+
+    console.log({ method: req.method });
     const secret = searchParams.get("secret");
 
     if (secret === process.env.REVALIDATE_SECRET) {
